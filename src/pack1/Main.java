4381be8300b4b1e4578ee3f83a4f4d4e6d7c3d5c
liliya <liliya.fedyurina@gmail.com>
2021-02-22 16:52:26 -0800

    Longest Increasing Substring

---
+++ src/pack1/Main.java
@@ -0,0 +1,27 @@
+package pack1;
+
+public class Main {
+	public static void main(String[] args) {
+		int[] arr1 = { 1 };
+		System.out.println("Result 1: " + Solution.lis(arr1));
+
+		int[] arr2 = { -1 };
+		System.out.println("Result 2: " + Solution.lis(arr2));
+
+		int[] arr3 = { -1, 1 };
+		System.out.println("Result 3: " + Solution.lis(arr3));
+
+		int[] arr4 = { 1, -1 };
+		System.out.println("Result 4: " + Solution.lis(arr4));
+
+		int[] arr5 = { 28, 10, 20, 11, 10, 15, -1, 5 };
+		System.out.println("Result 5: " + Solution.lis(arr5));
+
+		int[] arr6 = { -5, -4, -3, -2, -1, -10 };
+		System.out.println("Result 6: " + Solution.lis(arr6));
+
+		int[] arr7 = { 5, 7, -24, 12, 10, 2, 3, 12, 5, 6, 35 };
+		System.out.println("Result 7: " + Solution.lis(arr7));
+	}
+
+}
